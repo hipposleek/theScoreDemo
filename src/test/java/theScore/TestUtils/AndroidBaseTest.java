683d8bd2587e8242fdf4da3d73de4e95f739ad88
Min Sung <msung177@gmail.com>
2024-05-06 15:51:53 -0700

    Added global properties file and organized files

--- src/test/java/theScore/TestUtils/AndroidBaseTest.java
+++ src/test/java/theScore/TestUtils/AndroidBaseTest.java
@@ -1,38 +1,38 @@
 package theScore.TestUtils;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
+import theScore.utils.AppiumUtils;
 
-public class AndroidBaseTest {
+public class AndroidBaseTest extends AppiumUtils {
 
 	public AndroidDriver driver;
-	public AppiumDriverLocalService service;
 
 	@BeforeClass
-	public void ConfigureAppium() throws MalformedURLException, URISyntaxException {
-		service = new AppiumServiceBuilder()
-				.withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
-				.withIPAddress("0.0.0.0").usingPort(4723).build();
-		service.start();
+	public void configureAppium() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//theScore//resources//data.properties");
+		prop.load(file);
+		
+		String ipAddress = prop.getProperty("ipAddress");
+		String port = prop.getProperty("port");
+		System.out.println(ipAddress);
+		
+		service = startAppiumServer(ipAddress, Integer.parseInt(port));
 
 		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("MinTestDevice");
-		options.setApp(
-				"//Users/minsung//eclipse-workspace//AppiumFramework//src//test//java//resources//theScore_24.8.0.apk");
-		options.setFullReset(true);
-		
-		driver = new AndroidDriver(new URI("http://0.0.0.0:4723").toURL(), options);
+		options.setDeviceName(prop.getProperty("AndroidDeviceName"));
+		options.setApp(System.getProperty("user.dir") + "//src//test//java//theScore//apps//theScore_24.8.0.apk");		
+
+		driver = new AndroidDriver(service.getUrl(), options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
